6720cb8ad688af47fe53ed651d27b069c9849d8e
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-05-05 22:37:38 +0530

    connecting database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const dbConncet = require("./utils/database");
+
+dbConncet();
 const port = process.env.PORT || 5050;
 
 app.listen(port, console.log(`Server listening on port ${port}`));
